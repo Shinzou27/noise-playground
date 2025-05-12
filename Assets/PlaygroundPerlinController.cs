07b7c263bc98a03be8ce10ec6c8963e1118018b4
Felipe <felipecassianobarbosa@hotmail.com>
2025-05-12 13:06:42 -0300

    prepare canvas building

---
+++ Assets/PlaygroundPerlinController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaygroundPerlinController : MonoBehaviour
+{
+    public enum FlowState {
+        MENU,
+        STEP_BY_STEP_2D,
+        SURFACE_GENERATOR,
+        GENERATOR_3D,
+    };
+    public FlowState current = FlowState.MENU;
+    [SerializeField] private GameObject menuContainer;
+    [SerializeField] private GameObject stepByStep2DContainer;
+    [SerializeField] private GameObject surfaceContainer;
+    [SerializeField] private GameObject generator3DContainer;
+    public void ChangeToMenu() {
+        current = FlowState.MENU;
+    }
+    public void ChangeTo2DStepByStep() {
+        current = FlowState.STEP_BY_STEP_2D;
+    }
+    public void ChangeToSurfaceGen() {
+        current = FlowState.SURFACE_GENERATOR;
+    }
+    public void Change3DGen() {
+        current = FlowState.GENERATOR_3D;
+    }
+    void Update() {
+        menuContainer.SetActive(current == FlowState.MENU);
+        stepByStep2DContainer.SetActive(current == FlowState.STEP_BY_STEP_2D);
+        surfaceContainer.SetActive(current == FlowState.SURFACE_GENERATOR);
+        generator3DContainer.SetActive(current == FlowState.GENERATOR_3D);
+    }
+}
